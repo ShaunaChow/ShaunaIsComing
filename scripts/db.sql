8f840c01474a09ba3810a4a0a6789290a8e2129c
ShaunaChow <z1023778132@icloud.com>
2020-11-23 22:34:47 +0800

    'sql'

--- scripts/db.sql
+++ scripts/db.sql
@@ -2,6 +2,7 @@ create database ShaunaIsComing;
 
 use ShaunaIsComing
 
+-- 创建用户表
 create table users (
   id int primary key  auto_increment,
   phonenum varchar(11) unique ,
@@ -32,3 +33,62 @@ values
 select * from users;
 
 update users set name='User' where id not in (1,2,3);
+
+
+-- 创建角色表
+create table roles (
+  id int primary key auto_increment,
+  roleName varchar(50) default null,
+  description varchar(255) default null,
+  createTime timestamp,
+  updateTime timestamp,
+  unique key unique_roleName (roleName)
+);
+
+-- 创建角色用户关联表
+create table user_role (
+  userId int not null ,
+  roleId int not null ,
+  createTime timestamp ,
+  creator varchar(255),
+  primary key (userId, roleId)
+);
+
+-- 创建权限表
+create table permission (
+  id int primary key auto_increment,
+  code varchar(32) not null comment '权限标识符',
+  description varchar(255) default null,
+  url varchar(128) not null comment 'url地址'
+);
+
+-- 创建角色权限关联表
+create table role_permission (
+  roleId int not null ,
+  permissionId int not null ,
+  createTime timestamp ,
+  creator varchar(255),
+  primary key (roleId, permissionId)
+);
+
+insert into roles
+(roleName,createTime,updateTime)
+values
+('System',now(),now()),
+('User',now(),now());
+
+insert into user_role
+(userId,roleId,createTime,creator)
+values
+(1,1,now(),'ShaunaChow'),
+(2,1,now(),'ShaunaChow'),
+(3,1,now(),'ShaunaChow'),
+(5,2,now(),'ShaunaChow'),
+(6,2,now(),'ShaunaChow'),
+(7,2,now(),'ShaunaChow');
+
+insert into permission
+(code,url)
+values
+('1','/r/r1'),
+('2','/r/r2');
